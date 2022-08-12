4f5bd7e9b6a2be769a62336809688a9c13953df8
Sergey Ryabinin <ryabinin.sergey@gmail.com>
2022-08-12 20:34:35 +0300

    tast complete

--- src/main/java/ru/odybo/meliso/Meliso.java
+++ src/main/java/ru/odybo/meliso/Meliso.java
@@ -4,6 +4,9 @@
  */
 package ru.odybo.meliso;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  *
  * @author vragos
@@ -12,19 +15,29 @@ package ru.odybo.meliso;
 
 public class Meliso {
 
+    public int sumOfStream(InputStream inputStream) throws IOException {
+        int mySummOfStream = 0;
+        int nRead = 0;
+        byte[] myData = new byte[8];
+        try (inputStream) {
+            while (( nRead = inputStream.read(myData, 0, myData.length)) != -1 ) {
+                for (int i = 0; i < myData.length; i++ ) {
+                    mySummOfStream += myData[i];
+                    myData[i] = 0;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            throw new IOException();
+        }
+        return mySummOfStream;
+    }
+
+
     public static void main(String args[]) {
-        String[] weekends = {"Friday", "Saturday", "Sunday"};
-        assert weekends.length == 3;
-        System.out.println("There are " + weekends.length + "  weekends in a week");
-        int[] myData = {1, 2, 3, 4};
-        for (int i = 0; i < myData.length; i++ ) {
-            System.out.println(":" + myData[i]);
-            myData[i] = 0;
-        }
-        System.out.println("***");
-        for (int item: myData) {
-            System.out.println(":" + item);
-            item = 0;
-        }
+
+
+
     }
 }
