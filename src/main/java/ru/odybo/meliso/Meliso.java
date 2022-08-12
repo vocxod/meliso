1e99d8000e91a4a8b5be24c036b885666e454f48
Sergey Ryabinin <ryabinin.sergey@gmail.com>
2022-08-12 14:41:56 +0300

    hierarsy sample with use interfaces

--- src/main/java/ru/odybo/meliso/Meliso.java
+++ src/main/java/ru/odybo/meliso/Meliso.java
@@ -2,16 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package ru.odybo.meliso;
 
 /**
  *
  * @author vragos
  */
+
+
 public class Meliso {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String args[]) {
+        String[] weekends = {"Friday", "Saturday", "Sunday"};
+        assert weekends.length == 3;
+        System.out.println("There are " + weekends.length + "  weekends in a week");
+
     }
 }
