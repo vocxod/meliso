7ea9813b6a4affabbbc707834e1bbbb4176128b5
Sergey Ryabinin <ryabinin.sergey@gmail.com>
2022-08-13 10:45:54 +0300

    5 3 11 Stream ready

--- src/main/java/ru/odybo/meliso/Meliso.java
+++ src/main/java/ru/odybo/meliso/Meliso.java
@@ -5,6 +5,7 @@
 package ru.odybo.meliso;
 
 import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -15,11 +16,45 @@ import java.nio.charset.StandardCharsets;
 
 public class Meliso {
 
+    public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
+        String retval;
+        Reader reader = new InputStreamReader(inputStream, charset);
+        StringWriter r = new StringWriter();
+        int b;
+        while ((b = reader.read()) != -1) {
+            r.write(b);
+        }
+        retval = r.toString();
+        return retval;
+
+    /*
+    // этот код по непонятным причинам не проходит тест
+    String currentLine;
+    StringBuilder myResult = new StringBuilder();
+    try (inputStream) {
+        BufferedReader bufferReader = new BufferedReader(
+                new InputStreamReader(inputStream, charset));
+        while ( (currentLine = bufferReader.readLine()) != null ) {
+            myResult.append(currentLine);
+            myResult.append(System.lineSeparator());
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+        System.out.println(e.getMessage());
+    }
+    // myResult.deleteCharAt(myResult.length() - 1);
+    return myResult.toString();
+    */
+    }
+
+
     public static void main(String args[]) throws IOException {
+        /*
         OutputStream outputStream       = new FileOutputStream("output_stream.txt");
         Writer writer = new OutputStreamWriter(outputStream, StandardCharsets.US_ASCII);
         // writer.append("QQЙ");
         writer.write('ф');
         writer.close();
+         */
     }
 }
